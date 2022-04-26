a2a70ae8b01478186a79c44760839e15cab81bcb
Milan-code-person <91362767+Milan-code-person@users.noreply.github.com>
2022-04-26 20:53:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+img=""
+status="";
+object = [];
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById("status").innerHTML = "Status : detecting objects";
+}
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+function draw(){
+    r = random(255);
+    g = random(255);
+    b = random(255);
+    image(video, 0, 0, 380, 380);
+    if(status !=""){
+        for (i = 0;i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            dpcument.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+object.lenght;
+            fill(r,g,b);
+            fill("#FF0000");
+            percent = floor(objects [i].confidence * 100);
+            text(objects[i].label+""+percent+"%",objects[i].x+15,objects[i].y+15);
+            nofill();
+            stroke(r,g,b);
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status= true;
+    objectDetector.detect(video, gotresult);
+}
+function gotresult(error, results){
+    if (error){
+        console.log(error); 
+    }
+    console.log(results);
+    object = results;
+}
